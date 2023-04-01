81e9ed64fca9f43bd32953ac4f7f40bab7bf0200
Keiji Yoshimi <walf443@gmail.com>
2023-04-01 16:38:59 +0900

    move get_user_info to util.rs

--- webapp/rust/isucholar/src/routes/user_routes.rs
+++ webapp/rust/isucholar/src/routes/user_routes.rs
@@ -1,28 +1,7 @@
 use crate::responses::error::SqlxError;
 use crate::responses::get_me_response::GetMeResponse;
 use actix_web::{web, HttpResponse};
-
-fn get_user_info(session: actix_session::Session) -> actix_web::Result<(String, String, bool)> {
-    let user_id = session.get("userID")?;
-    if user_id.is_none() {
-        return Err(actix_web::error::ErrorInternalServerError(
-            "failed to get userID from session",
-        ));
-    }
-    let user_name = session.get("userName")?;
-    if user_name.is_none() {
-        return Err(actix_web::error::ErrorInternalServerError(
-            "failed to get userName from session",
-        ));
-    }
-    let is_admin = session.get("isAdmin")?;
-    if is_admin.is_none() {
-        return Err(actix_web::error::ErrorInternalServerError(
-            "failed to get isAdmin from session",
-        ));
-    }
-    Ok((user_id.unwrap(), user_name.unwrap(), is_admin.unwrap()))
-}
+use crate::routes::util::get_user_info;
 
 // GET /api/users/me 自身の情報を取得
 pub async fn get_me(
---
+++ webapp/rust/isucholar/src/routes/util.rs
@@ -0,0 +1,22 @@
+pub fn get_user_info(session: actix_session::Session) -> actix_web::Result<(String, String, bool)> {
+    let user_id = session.get("userID")?;
+    if user_id.is_none() {
+        return Err(actix_web::error::ErrorInternalServerError(
+            "failed to get userID from session",
+        ));
+    }
+    let user_name = session.get("userName")?;
+    if user_name.is_none() {
+        return Err(actix_web::error::ErrorInternalServerError(
+            "failed to get userName from session",
+        ));
+    }
+    let is_admin = session.get("isAdmin")?;
+    if is_admin.is_none() {
+        return Err(actix_web::error::ErrorInternalServerError(
+            "failed to get isAdmin from session",
+        ));
+    }
+    Ok((user_id.unwrap(), user_name.unwrap(), is_admin.unwrap()))
+}
+
