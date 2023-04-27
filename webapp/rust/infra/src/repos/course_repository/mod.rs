4b1e52aa0461dec01874783cec39ea71999f76f5
Keiji Yoshimi <walf443@gmail.com>
2023-04-28 00:24:53 +0900

    added test for CourseRepositoryInfra::find_status_for_share_lock_by_id

--- webapp/rust/infra/src/repos/course_repository/mod.rs
+++ webapp/rust/infra/src/repos/course_repository/mod.rs
@@ -17,6 +17,8 @@ mod find_by_code;
 #[cfg(test)]
 mod find_for_share_lock_by_id;
 #[cfg(test)]
+mod find_status_for_share_lock_by_id;
+#[cfg(test)]
 mod find_with_teacher_by_id;
 #[cfg(test)]
 mod for_update_by_id;
