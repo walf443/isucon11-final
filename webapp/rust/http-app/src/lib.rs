9e8b6e2ebe43fe65ec6573cb3aa52d9425484ea3
Keiji Yoshimi <walf443@gmail.com>
2023-04-30 10:48:22 +0900

    extract build app logic

--- webapp/rust/http-app/src/lib.rs
+++ webapp/rust/http-app/src/lib.rs
@@ -1,1 +1,53 @@
+use actix_web::dev::{ServiceFactory, ServiceRequest, ServiceResponse};
+use actix_web::{Error, web};
+use actix_web::body::{BoxBody, EitherBody};
+use isucholar_core::db::DBPool;
+use isucholar_http_core::middleware::IsLoggedIn;
+use isucholar_http_core::routes::announcement_routes::get_announcement_routes;
+use isucholar_http_core::routes::login::login;
+use isucholar_http_core::routes::logout::logout;
+use isucholar_http_core::routes::user_routes::get_user_routes;
+use isucholar_infra::services::manager::ServiceManagerImpl;
+use crate::routes::course_routes::get_course_routes;
+use crate::routes::initialize::initialize;
+
 pub mod routes;
+
+const SESSION_NAME: &str = "isucholar_rust";
+
+pub fn create_app(
+    pool: DBPool,
+    service: ServiceManagerImpl,
+    session_key: &Vec<u8>
+) -> actix_web::App<
+    impl ServiceFactory<
+        ServiceRequest, Config = (),
+        Response = ServiceResponse<EitherBody<BoxBody>>,
+        Error = Error, InitError =()>
+    >
+{
+    let users_api = get_user_routes::<ServiceManagerImpl>();
+    let courses_api = get_course_routes::<ServiceManagerImpl>();
+    let announcements_api = get_announcement_routes::<ServiceManagerImpl>();
+
+    actix_web::App::new()
+        .app_data(web::Data::new(pool))
+        .app_data(web::Data::new(service))
+        // .wrap(actix_web::middleware::Logger::default())
+        .wrap(
+            actix_session::CookieSession::signed(&session_key)
+                .secure(false)
+                .name(SESSION_NAME)
+                .max_age(3600),
+        )
+        .route("/initialize", web::post().to(initialize))
+        .route("/login", web::post().to(login::<ServiceManagerImpl>))
+        .route("/logout", web::post().to(logout))
+        .service(
+            web::scope("/api")
+                .wrap(IsLoggedIn)
+                .service(users_api)
+                .service(courses_api)
+                .service(announcements_api),
+        )
+}
--- webapp/rust/http-app/src/main.rs
+++ webapp/rust/http-app/src/main.rs
@@ -1,16 +1,7 @@
-use actix_web::web;
+use isucholar_http_app::create_app;
 
-use isucholar_http_app::routes::course_routes::get_course_routes;
-use isucholar_http_app::routes::initialize::initialize;
-use isucholar_http_core::middleware::IsLoggedIn;
-use isucholar_http_core::routes::announcement_routes::get_announcement_routes;
-use isucholar_http_core::routes::login::login;
-use isucholar_http_core::routes::logout::logout;
-use isucholar_http_core::routes::user_routes::get_user_routes;
 use isucholar_infra::db::get_db_conn;
 use isucholar_infra::services::manager::ServiceManagerImpl;
-
-const SESSION_NAME: &str = "isucholar_rust";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -24,30 +15,8 @@ async fn main() -> std::io::Result<()> {
     session_key.resize(32, 0);
 
     let server = actix_web::HttpServer::new(move || {
-        let users_api = get_user_routes::<ServiceManagerImpl>();
-        let courses_api = get_course_routes::<ServiceManagerImpl>();
-        let announcements_api = get_announcement_routes::<ServiceManagerImpl>();
-
-        actix_web::App::new()
-            .app_data(web::Data::new(pool.clone()))
-            .app_data(web::Data::new(service.clone()))
-            .wrap(actix_web::middleware::Logger::default())
-            .wrap(
-                actix_session::CookieSession::signed(&session_key)
-                    .secure(false)
-                    .name(SESSION_NAME)
-                    .max_age(3600),
-            )
-            .route("/initialize", web::post().to(initialize))
-            .route("/login", web::post().to(login::<ServiceManagerImpl>))
-            .route("/logout", web::post().to(logout))
-            .service(
-                web::scope("/api")
-                    .wrap(IsLoggedIn)
-                    .service(users_api)
-                    .service(courses_api)
-                    .service(announcements_api),
-            )
+        let app = create_app(pool.clone(), service.clone(), &session_key);
+        app.wrap(actix_web::middleware::Logger::default())
     });
     if let Some(l) = listenfd::ListenFd::from_env().take_tcp_listener(0)? {
         server.listen(l)?
