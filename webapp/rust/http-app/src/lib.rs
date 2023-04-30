bbf1e37a2a7f2e326689e5125971099c99c751a2
Keiji Yoshimi <walf443@gmail.com>
2023-04-30 11:35:45 +0900

    create integration test

--- webapp/rust/http-app/src/lib.rs
+++ webapp/rust/http-app/src/lib.rs
@@ -1,6 +1,8 @@
-use actix_web::dev::{ServiceFactory, ServiceRequest, ServiceResponse};
-use actix_web::{Error, web};
+use crate::routes::course_routes::get_course_routes;
+use crate::routes::initialize::initialize;
 use actix_web::body::{BoxBody, EitherBody};
+use actix_web::dev::{ServiceFactory, ServiceRequest, ServiceResponse};
+use actix_web::{web, Error};
 use isucholar_core::db::DBPool;
 use isucholar_http_core::middleware::IsLoggedIn;
 use isucholar_http_core::routes::announcement_routes::get_announcement_routes;
@@ -8,8 +10,6 @@ use isucholar_http_core::routes::login::login;
 use isucholar_http_core::routes::logout::logout;
 use isucholar_http_core::routes::user_routes::get_user_routes;
 use isucholar_infra::services::manager::ServiceManagerImpl;
-use crate::routes::course_routes::get_course_routes;
-use crate::routes::initialize::initialize;
 
 pub mod routes;
 
@@ -20,11 +20,13 @@ pub fn create_app(
     service: ServiceManagerImpl,
 ) -> actix_web::App<
     impl ServiceFactory<
-        ServiceRequest, Config = (),
+        ServiceRequest,
+        Config = (),
         Response = ServiceResponse<EitherBody<BoxBody>>,
-        Error = Error, InitError =()>
-    >
-{
+        Error = Error,
+        InitError = (),
+    >,
+> {
     let users_api = get_user_routes::<ServiceManagerImpl>();
     let courses_api = get_course_routes::<ServiceManagerImpl>();
     let announcements_api = get_announcement_routes::<ServiceManagerImpl>();
@@ -53,4 +55,3 @@ pub fn create_app(
                 .service(announcements_api),
         )
 }
-
---
+++ webapp/rust/http-app/tests/routes_users.rs
@@ -0,0 +1,66 @@
+use actix_web::test;
+use isucholar_core::db::get_test_db_conn;
+use isucholar_http_app::create_app;
+use isucholar_infra::services::manager::ServiceManagerImpl;
+
+#[actix_web::test]
+#[should_panic(expected = "You are not logged in.")]
+async fn get_me() {
+    let db_pool = get_test_db_conn().await.unwrap();
+    let service = ServiceManagerImpl::new(db_pool.clone());
+
+    let app = create_app(db_pool, service);
+
+    let app = test::init_service(app).await;
+    let req = test::TestRequest::get().uri("/api/users/me").to_request();
+
+    test::call_service(&app, req).await;
+}
+
+#[actix_web::test]
+#[should_panic(expected = "You are not logged in.")]
+async fn get_me_courses() {
+    let db_pool = get_test_db_conn().await.unwrap();
+    let service = ServiceManagerImpl::new(db_pool.clone());
+
+    let app = create_app(db_pool, service);
+
+    let app = test::init_service(app).await;
+    let req = test::TestRequest::get()
+        .uri("/api/users/me/courses")
+        .to_request();
+
+    test::call_service(&app, req).await;
+}
+
+#[actix_web::test]
+#[should_panic(expected = "You are not logged in.")]
+async fn put_me_courses() {
+    let db_pool = get_test_db_conn().await.unwrap();
+    let service = ServiceManagerImpl::new(db_pool.clone());
+
+    let app = create_app(db_pool, service);
+
+    let app = test::init_service(app).await;
+    let req = test::TestRequest::put()
+        .uri("/api/users/me/courses")
+        .to_request();
+
+    test::call_service(&app, req).await;
+}
+
+#[actix_web::test]
+#[should_panic(expected = "You are not logged in.")]
+async fn get_me_grades() {
+    let db_pool = get_test_db_conn().await.unwrap();
+    let service = ServiceManagerImpl::new(db_pool.clone());
+
+    let app = create_app(db_pool, service);
+
+    let app = test::init_service(app).await;
+    let req = test::TestRequest::get()
+        .uri("/api/users/me/grades")
+        .to_request();
+
+    test::call_service(&app, req).await;
+}
